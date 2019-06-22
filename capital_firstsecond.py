ebc1be5b25a030d25004f9ad705a78977596a0d0
Deepika <32121254+Deepi20@users.noreply.github.com>
2019-06-21 22:11:25 -0500

    Create capital_firstsecond.py

---
+++ capital_firstsecond.py
@@ -0,0 +1,6 @@
+def capFirst_Secondname():
+	firstName =  input("Please input your first name")
+	secondName = input("Please input your second name")
+	firstName = firstName.capitalize()  
+	secondName = secondName.capitalize() 
+	print ("Your name capitalized is: ",firstName,secondName )
