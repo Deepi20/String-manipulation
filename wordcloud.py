de331ae2f95937f573271aa1b9daa01cf8fccaf2
Deepika <32121254+Deepi20@users.noreply.github.com>
2019-06-25 22:48:50 -0500

    Create wordcloud.py

---
+++ wordcloud.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud
+
+word_freq = gen_freq(dataset.text.str)
+
+wc = WordCloud(width= 400    ,height= 330    , max_words = 100, background_color = 'white').generate_from_frequencies(word_freq)
+
+plt.figure(figsize = (12,8))
+plt.imshow(wc,interpolation = 'bilinear')
+plt.axis('off')
+plt.show()
