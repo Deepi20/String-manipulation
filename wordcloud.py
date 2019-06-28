41c6dbc476ac446b89cbecaa93f5ef779bd9cb0f
Deepika <32121254+Deepi20@users.noreply.github.com>
2019-06-27 23:25:55 -0500

    Update wordcloud.py

--- wordcloud.py
+++ wordcloud.py
@@ -3,9 +3,9 @@ from wordcloud import WordCloud
 
 word_freq = gen_freq(dataset.text.str)
 
-wc = WordCloud(width= 400,height= 330, max_words = 100, background_color = 'white').generate_from_frequencies(word_freq)
+wc = WordCloud(width=900 ,height= 550, max_words = 100, background_color = 'white').generate_from_frequencies(word_freq)
 
-plt.figure(figsize = (12,8))
+plt.figure(figsize = (15,15))
 plt.imshow(wc,interpolation = 'bilinear')
 plt.axis('off')
 plt.show()
